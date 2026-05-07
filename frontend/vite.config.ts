1fb3aa9a5f68beaf887e2de73e9d33d419c2a293
willthanhtrann-dev <willthanhtrann@gmail.com>
2026-05-07 16:57:17 +0700

    init(shadcn): add shadcn ui with lura theme, modify tailwind and vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,13 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
